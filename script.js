1766404431eaaf2ed0391d6f39f275109fc6bf1a
Erzhan <erzhik68@mail.ru>
2023-04-15 17:44:00 +0500

    While instead of For

--- script.js
+++ script.js
@@ -10,7 +10,25 @@ const appData = {
   savings: false,
 };
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//   let itemOfExpenses = prompt(
+//       "Введите обязательную статью расходов в этом месяце",
+//       ""
+//     ),
+//     moneyItemOfExpenses = +prompt("Во сколько обойдется?", "");
+
+//   if (
+//     typeof itemOfExpenses != null &&
+//     typeof moneyItemOfExpenses != null &&
+//     itemOfExpenses != "" &&
+//     moneyItemOfExpenses != ""
+//   ) {
+//     appData.expenses[itemOfExpenses] = moneyItemOfExpenses;
+//     console.log("done");
+//   }
+// }
+let i = 0;
+while (i < 2) {
   let itemOfExpenses = prompt(
       "Введите обязательную статью расходов в этом месяце",
       ""
@@ -26,6 +44,7 @@ for (let i = 0; i < 2; i++) {
     appData.expenses[itemOfExpenses] = moneyItemOfExpenses;
     console.log("done");
   }
+  i++;
 }
 
 appData.moneyPerDay = (appData.budget / 30).toFixed();
